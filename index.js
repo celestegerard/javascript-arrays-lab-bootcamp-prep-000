8e9c99cf6fc39a82609d5c8f919798c88d3de03f
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-25 01:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function appendKitten(name){
  }
 
 function prependKitten(name) {
-  kittens = ["Arnold", ...kittens];
-  return kittens;
+  return [name, ...kittens];
+  
 }
 
 function removeLastKitten() {
