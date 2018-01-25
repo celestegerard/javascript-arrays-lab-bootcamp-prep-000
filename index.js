a6d94ccff25237f1731a9660253a2519ff0c5710
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-25 00:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,42 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
 function appendKitten(name) {
-  kittens = [...kittens, "Broom"];
-  return kittens;
+  appends a kitten to the kittens array
+  returns a new array;
+  this is a non-destructive method
+}
+
+function prependKitten(name) {
+  prepends a kitten to the kittens array 
+  returns a new array;
+  this is a non-destructive method;
+}
+
+function removeLastKitten() {
+  removes the last kitten in the kittens array
+  returns a new array;
+  this is a non-destructive method;
+}
+
+function removeFirstKitten() {
+  removes the first kitten from the kittens array
+  returns a new array;
+  this is a non-destructive method;
 }
