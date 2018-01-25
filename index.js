06fadc0b6b9859ddd5c4390d269c3c5f15bb6ee6
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-25 00:54:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(kittens, name) {
-  kittens = [...kittens, name];
-  return kittens;
+  return [...kittens, name];
 }
 
 function prependKitten(name) {
